b869d6c4785b9a6d2e94682136c3ac80773982f7
Mike Christiansen <mikechristiansen2000@gmail.com>
2019-04-25 07:37:18 -0500

    added comments

--- testSrc/cs3354/unitTestDemo/simpleCalc/SimpleCalcTestCase.java
+++ testSrc/cs3354/unitTestDemo/simpleCalc/SimpleCalcTestCase.java
@@ -3,7 +3,9 @@ package cs3354.unitTestDemo.simpleCalc;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import cs3354.unitTestDemo.util.CalcError;
@@ -12,20 +14,36 @@ public class SimpleCalcTestCase
 {
     private SimpleCalc calcUnderTest;
     
+    @BeforeClass
+    public static void startUp() throws Exception
+    {
+        System.out.println("SimpleCalc startUp");
+    }
+    
+    @AfterClass
+    public static void shutDown() throws Exception
+    {
+        System.out.println("SimpleCalc shutDown");
+    }
+
     @Before
     public void setUp() throws Exception
     {
+        System.out.println("SimpleCalc setUp");
         calcUnderTest = new SimpleCalc();
     }
 
     @After
     public void tearDown() throws Exception
     {
+        System.out.println("SimpleCalc tearDown");
     }
 
     @Test
     public void testAdd()
     {
+        System.out.println("SimpleCalc testAdd");
+
         double result = calcUnderTest.add(1,1);
         assertEquals(2.0, result, .001);
     }
@@ -33,6 +51,8 @@ public class SimpleCalcTestCase
     @Test
     public void testSub()
     {
+        System.out.println("SimpleCalc testSub");
+
         double result = calcUnderTest.sub(2,1);
         assertEquals(1.0, result, .001);
     }
@@ -40,6 +60,8 @@ public class SimpleCalcTestCase
     @Test
     public void testMul()
     {
+        System.out.println("SimpleCalc testMul");
+
         double result = calcUnderTest.mul(2,1);
         assertEquals(2.0, result, .001);
     }
@@ -47,6 +69,8 @@ public class SimpleCalcTestCase
     @Test
     public void testDiv()
     {
+        System.out.println("SimpleCalc testDiv");
+
         double result = calcUnderTest.div(5,2);
         assertEquals(2.5, result, .001);
     }
@@ -54,6 +78,8 @@ public class SimpleCalcTestCase
     @Test(expected=CalcError.class)
     public void testDivError()
     {
+        System.out.println("SimpleCalc testDivError");
+
         double result = calcUnderTest.div(1, 0);
     }
 
